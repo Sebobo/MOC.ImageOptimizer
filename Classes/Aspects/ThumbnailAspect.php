fe907213ed8d13c3fd6f839b14ee3f19d791df8d
Sebastian Helzle <sebastian@helzle.it>
2017-01-18 23:36:05 +0100

    TASK: Use better libs for image optimization

--- Classes/Aspects/ThumbnailAspect.php
+++ Classes/Aspects/ThumbnailAspect.php
@@ -11,120 +11,124 @@ use TYPO3\Flow\Resource\ResourceManager;
  * @Flow\Scope("singleton")
  * @Flow\Aspect
  */
-class ThumbnailAspect {
+class ThumbnailAspect
+{
 
-	/**
-	 * @var SystemLoggerInterface
-	 * @Flow\Inject
-	 */
-	protected $systemLogger;
+    /**
+     * @var SystemLoggerInterface
+     * @Flow\Inject
+     */
+    protected $systemLogger;
 
-	/**
-	 * @Flow\Inject
-	 * @var PackageManagerInterface
-	 */
-	protected $packageManager;
+    /**
+     * @Flow\Inject
+     * @var PackageManagerInterface
+     */
+    protected $packageManager;
 
-	/**
-	 * @Flow\Inject
-	 * @var ResourceManager
-	 */
-	protected $resourceManager;
+    /**
+     * @Flow\Inject
+     * @var ResourceManager
+     */
+    protected $resourceManager;
 
-	/**
-	 * @var array
-	 */
-	protected $settings;
+    /**
+     * @var array
+     */
+    protected $settings;
 
-	/**
-	 * @param array $settings
-	 * @return void
-	 */
-	public function injectSettings(array $settings) {
-		$this->settings = $settings;
-	}
+    /**
+     * @param array $settings
+     * @return void
+     */
+    public function injectSettings(array $settings)
+    {
+        $this->settings = $settings;
+    }
 
-	/**
-	 * After a thumbnail has been refreshed the resource is optimized, meaning the
-	 * image is only optimized once when created.
-	 *
-	 * A new resource is generated for every thumbnail, meaning the original is
-	 * never touched.
-	 *
-	 * Only local file system target is supported to keep it from being blocking.
-	 * It would however be possible to create a local copy of the resource,
-	 * process it, import it and set that as the thumbnail resource.
-	 *
-	 * @Flow\AfterReturning("method(TYPO3\Media\Domain\Model\Thumbnail->refresh())")
-	 * @param \TYPO3\Flow\Aop\JoinPointInterface $joinPoint The current join point
-	 * @return void
-	 */
-	public function optimizeThumbnail(JoinPointInterface $joinPoint) {
-		/** @var \TYPO3\Media\Domain\Model\Thumbnail $thumbnail */
-		$thumbnail = $joinPoint->getProxy();
-		$thumbnailResource = $thumbnail->getResource();
-		if (!$thumbnailResource) {
-                        return;
-		}
+    /**
+     * After a thumbnail has been refreshed the resource is optimized, meaning the
+     * image is only optimized once when created.
+     *
+     * A new resource is generated for every thumbnail, meaning the original is
+     * never touched.
+     *
+     * Only local file system target is supported to keep it from being blocking.
+     * It would however be possible to create a local copy of the resource,
+     * process it, import it and set that as the thumbnail resource.
+     *
+     * @Flow\AfterReturning("method(TYPO3\Media\Domain\Model\Thumbnail->refresh())")
+     * @param \TYPO3\Flow\Aop\JoinPointInterface $joinPoint The current join point
+     * @return void
+     */
+    public function optimizeThumbnail(JoinPointInterface $joinPoint)
+    {
+        /** @var \TYPO3\Media\Domain\Model\Thumbnail $thumbnail */
+        $thumbnail = $joinPoint->getProxy();
+        $thumbnailResource = $thumbnail->getResource();
+        if (!$thumbnailResource) {
+            return;
+        }
 
-		$streamMetaData = stream_get_meta_data($thumbnailResource->getStream());
-		$pathAndFilename = $streamMetaData['uri'];
+        $streamMetaData = stream_get_meta_data($thumbnailResource->getStream());
+        $pathAndFilename = $streamMetaData['uri'];
 
-		$useGlobalBinary = $this->settings['useGlobalBinary'];
-		$binaryRootPath = 'Private/Library/node_modules/';
-		$file = escapeshellarg($pathAndFilename);
-		$imageType = $thumbnailResource->getMediaType();
-		switch ($imageType) {
-			case 'image/jpeg':
-				if ($this->settings['formats']['jpg']['enabled'] === FALSE) {
-					return;
-				}
-				$library = 'jpegtran';
-				$binaryPath = sprintf('%1$s-bin/vendor/%s', $library);
-				$arguments = sprintf('-copy none -optimize %s -outfile %s %s', $this->settings['formats']['jpg']['progressive'] === TRUE ? '-progressive' : '', $file, $file);
-				if ($this->settings['formats']['jpg']['useGlobalBinary'] === TRUE) {
-					$useGlobalBinary = TRUE;
-				}
-				break;
-			case 'image/png':
-				if ($this->settings['formats']['png']['enabled'] === FALSE) {
-					return;
-				}
-				$library = 'optipng';
-				$binaryPath = sprintf('%1$s-bin/vendor/%s', $library);
-				$arguments = sprintf('-o%u -strip all -out %s %s', $this->settings['formats']['png']['optimizationLevel'], $file, $file);
-				if ($this->settings['formats']['png']['useGlobalBinary'] === TRUE) {
-					$useGlobalBinary = TRUE;
-				}
-				break;
-			case 'image/gif':
-				if ($this->settings['formats']['gif']['enabled'] === FALSE) {
-					return;
-				}
-				$library = 'gifsicle';
-				$binaryPath = sprintf('%1$s/vendor/%1$s', $library);
-				$arguments = sprintf('--batch -O%u %s ', $this->settings['formats']['gif']['optimizationLevel'], $file);
-				if ($this->settings['formats']['gif']['useGlobalBinary'] === TRUE) {
-					$useGlobalBinary = TRUE;
-				}
-				break;
-			case 'image/svg+xml':
-				if ($this->settings['formats']['svg']['enabled'] === FALSE) {
-					return;
-				}
-				$library = 'svgo';
-				$binaryPath = sprintf('%1$s/bin/%1$s', $library);
-				$arguments = sprintf('%s %s', $this->settings['formats']['svg']['pretty'] === TRUE ? '--pretty' : '', $file);
-				if ($this->settings['formats']['svg']['useGlobalBinary'] === TRUE) {
-					$useGlobalBinary = TRUE;
-				}
-				break;
-		}
-		$binaryPath = $useGlobalBinary === TRUE ? $this->settings['globalBinaryPath'] . $library : $this->packageManager->getPackageOfObject($this)->getResourcesPath() . $binaryRootPath . $binaryPath;
-		$cmd = escapeshellcmd($binaryPath) . ' ' . $arguments;
-		$output = [];
-		exec($cmd, $output, $result);
-		$this->systemLogger->log($cmd . ' (' . ((int)$result === 0 ? 'OK' : 'Error: ' . $result) . ')', LOG_INFO, $output);
-	}
+        $useGlobalBinary = $this->settings['useGlobalBinary'];
+        $binaryRootPath = 'Private/Library/node_modules/';
+        $file = escapeshellarg($pathAndFilename);
+        $imageType = $thumbnailResource->getMediaType();
+
+        switch ($imageType) {
+            case 'image/jpeg':
+                if ($this->settings['formats']['jpg']['enabled'] === FALSE) {
+                    return;
+                }
+                $library = 'jpegoptim';
+                $binaryPath = sprintf('%1$s-bin/vendor/%s', $library);
+                $arguments = sprintf('--strip-all --max=80 --all-progressive -o %s', $file, $file);
+                if ($this->settings['formats']['jpg']['useGlobalBinary'] === TRUE) {
+                    $useGlobalBinary = TRUE;
+                }
+                break;
+            case 'image/png':
+                if ($this->settings['formats']['png']['enabled'] === FALSE) {
+                    return;
+                }
+                $library = 'pngquant';
+                $binaryPath = sprintf('%1$s-bin/vendor/%s', $library);
+                $arguments = sprintf('-o %s %s', $file, $file);
+                if ($this->settings['formats']['png']['useGlobalBinary'] === TRUE) {
+                    $useGlobalBinary = TRUE;
+                }
+                break;
+            case 'image/gif':
+                if ($this->settings['formats']['gif']['enabled'] === FALSE) {
+                    return;
+                }
+                $library = 'gifsicle';
+                $binaryPath = sprintf('%1$s/vendor/%1$s', $library);
+                $arguments = sprintf('--batch -O%u %s ', $this->settings['formats']['gif']['optimizationLevel'], $file);
+                if ($this->settings['formats']['gif']['useGlobalBinary'] === TRUE) {
+                    $useGlobalBinary = TRUE;
+                }
+                break;
+            case 'image/svg+xml':
+                if ($this->settings['formats']['svg']['enabled'] === FALSE) {
+                    return;
+                }
+                $library = 'svgo';
+                $binaryPath = sprintf('%1$s/bin/%1$s', $library);
+                $arguments = sprintf('%s %s', $this->settings['formats']['svg']['pretty'] === TRUE ? '--pretty' : '', $file);
+                if ($this->settings['formats']['svg']['useGlobalBinary'] === TRUE) {
+                    $useGlobalBinary = TRUE;
+                }
+                break;
+        }
+        $binaryPath = $useGlobalBinary === TRUE ? $this->settings['globalBinaryPath'] . $library : $this->packageManager->getPackageOfObject($this)->getResourcesPath() . $binaryRootPath . $binaryPath;
+        $cmd = escapeshellcmd($binaryPath) . ' ' . $arguments;
+        $output = [];
+        exec($cmd, $output, $result);
+        $this->systemLogger->log($cmd . ' (' . ((int)$result === 0 ? 'OK' : 'Error: ' . $result) . ')', LOG_INFO, $output);
+    }
 
 }
